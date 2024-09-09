a5505985adfcedba280b57a283f97a84e0805a7a
abhas101 <abhaskumarmastermind@gmail.com>
2024-09-09 05:30:14 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base:"TakeoverAi",
+  "scripts": {
+    "build": "vite build",
+    "preview": "vite preview"
+  }
 })
