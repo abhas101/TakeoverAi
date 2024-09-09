dfcec6c00252829aad3b382699047652eda43f44
abhas101 <abhaskumarmastermind@gmail.com>
2024-09-09 08:11:11 +0530

    path

--- src/components/EntryHero.jsx
+++ src/components/EntryHero.jsx
@@ -6,7 +6,7 @@ const EntryHero = () => {
 
     <div className="heroWrapper p-2">
         <div className="heroContent flex justify-center">
-            <img src="/src/assets/robot.jpg" alt="robot" className='rounded-md shadow-md' />
+            <img src="./src/assets/robot.jpg" alt="robot" className='rounded-md shadow-md' />
         </div>
         <div className="heroTitle">
             <h2 className='text-5xl text-center font-medium mt-5'>Boost your Productivity</h2>
